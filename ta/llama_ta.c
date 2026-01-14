61e4bd805eab02421b5552a3778909997878c028
soyWXY <johnny852321@gapp.nthu.edu.tw>
2026-01-14 12:55:31 +0000

    resolve compiler warnings for llama_ta.c

--- ta/llama_ta.c
+++ ta/llama_ta.c
@@ -81,7 +81,7 @@ static size_t get_run_state_nbytes(Config* p) {
     return ret;
 }
 
-void malloc_run_state(RunState* s, Config* p, float *flat_buf) {
+static void malloc_run_state(RunState* s, Config* p, float *flat_buf) {
     int kv_dim = (p->dim * p->n_kv_heads) / p->n_heads;
     s->x = flat_buf; flat_buf += p->dim;
     s->xb = flat_buf; flat_buf += p->dim;
@@ -95,10 +95,10 @@ void malloc_run_state(RunState* s, Config* p, float *flat_buf) {
     s->logits = flat_buf; flat_buf += p->vocab_size;
 }
 
-void free_run_state(RunState* s) {
+static void free_run_state(RunState* s) {
 }
 
-void memory_map_weights(TransformerWeights *w, Config* p, float* ptr, int shared_weights) {
+static void memory_map_weights(TransformerWeights *w, Config* p, float* ptr, int shared_weights) {
     int head_size = p->dim / p->n_heads;
     // make sure the multiplications below are done in 64bit to fit the parameter counts of 13B+ models
     unsigned long long n_layers = p->n_layers;
@@ -129,32 +129,32 @@ void memory_map_weights(TransformerWeights *w, Config* p, float* ptr, int shared
     w->wcls = shared_weights ? w->token_embedding_table : ptr;
 }
 
-void read_weight(float *model_bin, Config* config, TransformerWeights* weights) {
+static void read_weight(float *model_bin, Config* config, TransformerWeights* weights) {
     // negative vocab size is hacky way of signaling unshared weights. bit yikes.
     int shared_weights = ((Config*)model_bin)->vocab_size > 0 ? 1 : 0;
     float* weights_ptr = model_bin + sizeof(Config)/sizeof(float);
     memory_map_weights(weights, config, weights_ptr, shared_weights);
 }
 
-void read_config(float *model_bin, Config* config) {
+static void read_config(float *model_bin, Config* config) {
     // read in the config header
     memcpy(config, model_bin, sizeof(Config));
     // negative vocab size is hacky way of signaling unshared weights. bit yikes.
     config->vocab_size = abs(config->vocab_size);
 }
 
-void build_transformer(Transformer *t, float* model_bin) {
+static void build_transformer(Transformer *t, float* model_bin) {
     // read in the Config from the checkpoint
     read_config(model_bin, &t->config);
 }
 
-void free_transformer(Transformer* t) {
+static void free_transformer(Transformer* t) {
 }
 
 // ----------------------------------------------------------------------------
 // neural net blocks; the dynamics of the Transformer
 
-void rmsnorm(float* o, float* x, float* weight, int size) {
+static void rmsnorm(float* o, float* x, float* weight, int size) {
     // calculate sum of squares
     float ss = 0.0f;
     for (int j = 0; j < size; j++) {
@@ -169,7 +169,7 @@ void rmsnorm(float* o, float* x, float* weight, int size) {
     }
 }
 
-void softmax(float* x, int size) {
+static void softmax(float* x, int size) {
     // find max value (for numerical stability)
     float max_val = x[0];
     for (int i = 1; i < size; i++) {
@@ -189,11 +189,10 @@ void softmax(float* x, int size) {
     }
 }
 
-void matmul(float* xout, float* x, float* w, int n, int d) {
+static void matmul(float* xout, float* x, float* w, int n, int d) {
     // W (d,n) @ x (n,) -> xout (d,)
     // by far the most amount of time is spent inside this little function
     int i;
-    #pragma omp parallel for private(i)
     for (i = 0; i < d; i++) {
         float val = 0.0f;
         for (int j = 0; j < n; j++) {
@@ -203,7 +202,7 @@ void matmul(float* xout, float* x, float* w, int n, int d) {
     }
 }
 
-float* forward(Transformer* transformer, int token, int pos) {
+static float* forward(Transformer* transformer, int token, int pos) {
 
     // a few convenience variables
     Config* p = &transformer->config;
@@ -255,7 +254,6 @@ float* forward(Transformer* transformer, int token, int pos) {
 
         // multihead attention. iterate over all heads
         int h;
-        #pragma omp parallel for private(h)
         for (h = 0; h < p->n_heads; h++) {
             // get the query vector for this head
             float* q = s->q + h * head_size;
@@ -353,7 +351,7 @@ typedef struct {
     unsigned long long rng_state;
 } Sampler;
 
-int sample_argmax(float* probabilities, int n) {
+static int sample_argmax(float* probabilities, int n) {
     // return the index that has the highest probability
     int max_i = 0;
     float max_p = probabilities[0];
@@ -366,7 +364,7 @@ int sample_argmax(float* probabilities, int n) {
     return max_i;
 }
 
-int sample_mult(float* probabilities, int n, float coin) {
+static int sample_mult(float* probabilities, int n, float coin) {
     // sample index from probabilities (they must sum to 1!)
     // coin is a random number in [0, 1), usually from random_f32()
     float cdf = 0.0f;
@@ -379,7 +377,7 @@ int sample_mult(float* probabilities, int n, float coin) {
     return n - 1; // in case of rounding errors
 }
 
-int compare(const void* a, const void* b) {
+static int compare(const void* a, const void* b) {
     ProbIndex* a_ = (ProbIndex*) a;
     ProbIndex* b_ = (ProbIndex*) b;
     if (a_->prob > b_->prob) return -1;
@@ -387,7 +385,7 @@ int compare(const void* a, const void* b) {
     return 0;
 }
 
-int sample_topp(float* probabilities, int n, float topp, ProbIndex* probindex, float coin) {
+static int sample_topp(float* probabilities, int n, float topp, ProbIndex* probindex, float coin) {
     // top-p sampling (or "nucleus sampling") samples from the smallest set of
     // tokens that exceed probability topp. This way we never sample tokens that
     // have very low probabilities and are less likely to go "off the rails".
@@ -430,7 +428,7 @@ int sample_topp(float* probabilities, int n, float topp, ProbIndex* probindex, f
     return probindex[last_idx].index; // in case of rounding errors
 }
 
-void build_sampler(Sampler* sampler, int vocab_size, float temperature, float topp, unsigned long long rng_seed) {
+static void build_sampler(Sampler* sampler, int vocab_size, float temperature, float topp, unsigned long long rng_seed) {
     sampler->vocab_size = vocab_size;
     sampler->temperature = temperature;
     sampler->topp = topp;
@@ -439,22 +437,22 @@ void build_sampler(Sampler* sampler, int vocab_size, float temperature, float to
     sampler->probindex = malloc(sampler->vocab_size * sizeof(ProbIndex));
 }
 
-void free_sampler(Sampler* sampler) {
+static void free_sampler(Sampler* sampler) {
     free(sampler->probindex);
 }
 
-unsigned int random_u32(unsigned long long *state) {
+static unsigned int random_u32(unsigned long long *state) {
     // xorshift rng: https://en.wikipedia.org/wiki/Xorshift#xorshift.2A
     *state ^= *state >> 12;
     *state ^= *state << 25;
     *state ^= *state >> 27;
     return (*state * 0x2545F4914F6CDD1Dull) >> 32;
 }
-float random_f32(unsigned long long *state) { // random float32 in [0,1)
+static float random_f32(unsigned long long *state) { // random float32 in [0,1)
     return (random_u32(state) >> 8) / 16777216.0f;
 }
 
-int sample(Sampler* sampler, float* logits) {
+static int sample(Sampler* sampler, float* logits) {
     // sample the token given the logits and some hyperparameters
     int next;
     if (sampler->temperature == 0.0f) {
