d7fa0c16b5599a2fcbb59fc49b50f1174468aeb7
soyWXY <johnny852321@gapp.nthu.edu.tw>
2026-04-30 04:36:36 +0000

    increase TA heap for 110M model

--- ta/user_ta_header_defines.h
+++ ta/user_ta_header_defines.h
@@ -21,7 +21,7 @@
 #define TA_STACK_SIZE			(2 * 1024)
 
 /* Provisioned heap size for TEE_Malloc() and friends */
-#define TA_DATA_SIZE			(200 * 1024 * 1024)
+#define TA_DATA_SIZE			(500 * 1024 * 1024)
 
 /* The gpd.ta.version property */
 #define TA_VERSION	"1.0"
